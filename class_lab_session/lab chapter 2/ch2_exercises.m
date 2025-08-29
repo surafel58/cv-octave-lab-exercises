ba7228ff808bbbc2fa53432d1026e7c74a24ed1f
surafel <surafel.s.tadesse@gmail.com>
2025-08-29 21:17:09 +0300

    update lab 2

--- class_lab_session/lab chapter 2/ch2_exercises.m
+++ class_lab_session/lab chapter 2/ch2_exercises.m
@@ -3,11 +3,19 @@
 % Load color image
 color_img = imread('../images/peppers.png');
 
+fprintf('=== EXERCISE 1: RGB to Grayscale Conversion ===\n');
+
 % Exercise 1: Convert RGB to grayscale using luminosity method
 gray_img = 0.299 * color_img(:,:,1) + 0.587 * color_img(:,:,2) + 0.114 * color_img(:,:,3);
-figure;
-imshow(uint8(gray_img));
-title('Grayscale - Luminosity Method');
+
+% Display Exercise 1 results grouped together
+figure('Name', 'Exercise 1: RGB to Grayscale Conversion', 'Position', [100, 100, 800, 400]);
+subplot(1,2,1); imshow(color_img); title('Original Color Image');
+subplot(1,2,2); imshow(uint8(gray_img)); title('Grayscale - Luminosity Method');
+
+fprintf('Exercise 1 completed: RGB to grayscale using luminosity method\n');
+
+fprintf('\n=== EXERCISE 3: Histogram Equalization ===\n');
 
 % Exercise 3: Histogram equalization on each channel separately
 R = color_img(:,:,1);
@@ -19,9 +27,15 @@ G_eq = histeq(G);
 B_eq = histeq(B);
 
 equalized_img = cat(3, R_eq, G_eq, B_eq);
-figure;
-imshow(equalized_img);
-title('Histogram Equalized Image');
 
+% Display Exercise 3 results grouped together
+figure('Name', 'Exercise 3: Histogram Equalization', 'Position', [150, 150, 800, 400]);
+subplot(1,2,1); imshow(color_img); title('Original Color Image');
+subplot(1,2,2); imshow(equalized_img); title('Histogram Equalized Image');
+
+fprintf('Exercise 3 completed: Histogram equalization on each RGB channel\n');
+
+fprintf('\n=== EXERCISE 2: Interactive Brightness Adjustment ===\n');
 % Exercise 2: Interactive brightness adjustment (run separately)
 % Run: ch2_interactive_brightness
+fprintf('Run ch2_interactive_brightness for interactive brightness control\n');
--- class_lab_session/lab chapter 2/ch2_practice2.m
+++ class_lab_session/lab chapter 2/ch2_practice2.m
@@ -5,25 +5,21 @@ color_img = imread('../images/peppers.png');
 
 % 2.2 Brightness Increase
 bright_img = color_img + 50;
-figure;
-imshow(bright_img);
-title('Brightness Increased');
 
 % 2.3 Brightness Decrease
 dark_img = color_img - 50;
-figure;
-imshow(dark_img);
-title('Brightness Decreased');
 
 % 2.4 Image Negative
 negative_img = 255 - color_img;
-figure;
-imshow(negative_img);
-title('Negative Image');
 
 % 2.5 Contrast Stretching
 double_img = im2double(color_img);
 contrast_img = imadjust(double_img, stretchlim(double_img), []);
-figure;
-imshow(contrast_img);
-title('Contrast Stretched Image');
+
+% Display all results in one figure
+figure('Position', [100, 100, 1200, 800]);
+subplot(2,3,1); imshow(color_img); title('Original Image');
+subplot(2,3,2); imshow(bright_img); title('Brightness Increased');
+subplot(2,3,3); imshow(dark_img); title('Brightness Decreased');
+subplot(2,3,4); imshow(negative_img); title('Negative Image');
+subplot(2,3,5); imshow(contrast_img); title('Contrast Stretched Image');
